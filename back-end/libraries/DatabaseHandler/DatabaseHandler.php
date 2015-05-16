b3de215dd2f4ba9a0f73efe04c936e3e54af6786
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-05-16 13:24:57 -0300

    esthetics changes

--- back-end/libraries/DatabaseHandler/DatabaseHandler.php
+++ back-end/libraries/DatabaseHandler/DatabaseHandler.php
@@ -25,15 +25,15 @@
 class DatabaseHandler implements IDatabaseHandler
 {
 	/* attributes: */
-  	private static $instance;
-  	private $host;
-  	private $port;
-  	private $name;
-  	private $user;
-  	private $password;
-  	private $filePath;
-  	private $databaseObject;
-  	private $databaseLink;
+	private static $instance;
+	private $host;
+	private $port;
+	private $name;
+	private $user;
+	private $password;
+	private $filePath;
+	private $databaseObject;
+	private $databaseLink;
 
 	/* methods: */
 
@@ -48,17 +48,17 @@ class DatabaseHandler implements IDatabaseHandler
 		(
 			'Invalid Operation: You can not clone an instance of '
 			. get_class($this) ." class.", E_USER_ERROR 
-		);
+			);
 	}
 
 	//to prevent deserialization:
 	private function __wakeup()
 	{
-	  	trigger_error
-	  	(
-	  		'Invalid Operation: You can not deserialize an instance of '
-	  		. get_class($this) ." class."
-	  	);
+		trigger_error
+		(
+			'Invalid Operation: You can not deserialize an instance of '
+			. get_class($this) ." class."
+			);
 	}
 
 	public static function getInstance()
@@ -71,131 +71,131 @@ class DatabaseHandler implements IDatabaseHandler
 	}
 
 	/*FOR PROPERTIES*/
-    public function __get($name)
-    {
-        if (method_exists($this, ($method = 'get'.ucfirst($name))))
-        {
-          return $this->$method();
-        }
-        else return;
-    }
+	public function __get($name)
+	{
+		if (method_exists($this, ($method = 'get'.ucfirst($name))))
+		{
+			return $this->$method();
+		}
+		else return;
+	}
 
-    public function __set($name, $value)
-    {
-        if (method_exists($this, ($method = 'set'.ucfirst($name))))
-        {
-          $this->$method($value);
-        }
-    }
+	public function __set($name, $value)
+	{
+		if (method_exists($this, ($method = 'set'.ucfirst($name))))
+		{
+			$this->$method($value);
+		}
+	}
 	/*FOR PROPERTIES*/
 	
 
 	/*GETTERS AND SETTERS*/
-  	private function getHost()
-  	{
-  	    return $this->host;
-  	}
-  	
-  	private function setHost($host)
-  	{
-  	    $this->host = $host;
-  	    return $this;
-  	}
+	private function getHost()
+	{
+		return $this->host;
+	}
 
-  	private function getPort()
-  	{
-  	    return $this->port;
-  	}
-  	
-  	private function setPort($port)
-  	{
-  	    $this->port = $port;
-  	    return $this;
-  	}
+	private function setHost($host)
+	{
+		$this->host = $host;
+		return $this;
+	}
 
-  	private function getName()
-  	{
-  	    return $this->name;
-  	}
-  	
-  	private function setName($name)
-  	{
-  	    $this->name = $name;
-  	    return $this;
-  	}
+	private function getPort()
+	{
+		return $this->port;
+	}
 
-  	private function getUser()
-  	{
-  	    return $this->user;
-  	}
-  	
-  	private function setUser($user)
-  	{
-  	    $this->user = $user;
-  	    return $this;
-  	}
+	private function setPort($port)
+	{
+		$this->port = $port;
+		return $this;
+	}
 
-  	private function getPassword()
-  	{
-  	    return $this->password;
-  	}
-  	
-  	private function setPassword($password)
-  	{
-  	    $this->password = $password;
-  	    return $this;
-  	}
+	private function getName()
+	{
+		return $this->name;
+	}
 
-  	private function getFilePath()
-  	{
-  	    return $this->filePath;
-  	}
-  	
-  	private function setFilePath($filePath)
-  	{
-  	    $this->filePath = $filePath;
-  	    return $this;
-  	}
-  	/*GETTERS AND SETTERS*/
+	private function setName($name)
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	private function getUser()
+	{
+		return $this->user;
+	}
+
+	private function setUser($user)
+	{
+		$this->user = $user;
+		return $this;
+	}
+
+	private function getPassword()
+	{
+		return $this->password;
+	}
+
+	private function setPassword($password)
+	{
+		$this->password = $password;
+		return $this;
+	}
+
+	private function getFilePath()
+	{
+		return $this->filePath;
+	}
+
+	private function setFilePath($filePath)
+	{
+		$this->filePath = $filePath;
+		return $this;
+	}
+	/*GETTERS AND SETTERS*/
 
 	//LOAD DATABASE SYSTEM
 
   	//INTERFACE PUBLIC METHOD
-    public function openDBMS($systemName)
-    {
-    	$ucSystemName = ucfirst($systemName);
+	public function openDBMS($systemName)
+	{
+		$ucSystemName = ucfirst($systemName);
 
-    	require_once "{$ucSystemName}Database.php";
-    	
-    	$databaseClass = "{$ucSystemName}Database";
-    	$this -> databaseObject = new $databaseClass;
-    	$this -> databaseLink = $this -> databaseObject -> openDatabase();
-    	return $this;
-    }
+		require_once "{$ucSystemName}Database.php";
+
+		$databaseClass = "{$ucSystemName}Database";
+		$this -> databaseObject = new $databaseClass;
+		$this -> databaseLink = $this -> databaseObject -> openDatabase();
+		return $this;
+	}
     //INTERFACE PUBLIC METHOD
 
 	private function execQuery($query)
 	{ 
 		try 
-	    {
+		{
 			$this -> databaseLink -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$databaseReply = $this -> databaseLink -> prepare($query);
 			$databaseReply -> execute();
-	    }
+		}
 
-	    catch(PDOException $exception) 
-	    {
+		catch(PDOException $exception) 
+		{
 	    	//manage exception and log
-	      	echo $exception -> getMessage();  
-	    }
+			echo $exception -> getMessage();  
+		}
 
-	    return $databaseReply;
+		return $databaseReply;
 	}
 
 	private function execQueriesInTransaction($queries)
 	{ 
 		try 
-	    {
+		{
 			$this -> databaseLink -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this -> databaseLink -> beginTransaction();
 			foreach ($queries as $query) 
@@ -204,15 +204,15 @@ class DatabaseHandler implements IDatabaseHandler
 				$databaseReply -> execute();
 			}
 			$this -> databaseLink -> commit();
-	    }
+		}
 
-	    catch(PDOException $exception) 
-	    {
+		catch(PDOException $exception) 
+		{
 	    	//manage exception and log
-	      	echo $exception -> getMessage();  
-	    }
+			echo $exception -> getMessage();  
+		}
 
-	    return $databaseReply;
+		return $databaseReply;
 	}		
 
 	private function getResultingRowOnArray($databaseReply)
@@ -229,8 +229,8 @@ class DatabaseHandler implements IDatabaseHandler
 
 	private function closeDatabase($databaseLink, $databaseReply)
 	{
-	  	$databaseLink = null;
-	  	$databaseReply = null;
+		$databaseLink = null;
+		$databaseReply = null;
 	}
 
 //INTERFACE PUBLIC METHODS	
@@ -247,7 +247,7 @@ class DatabaseHandler implements IDatabaseHandler
 		$reply = $this -> execQuery($query);
 		$resultRow = $this -> getResultingRowOnArray($reply);
 		$this -> closeDatabase($databaseLink, $reply);
-	  	return $resultRow;
+		return $resultRow;
 	}
 
 	public function SQLFetchAllArray($query)
@@ -256,7 +256,7 @@ class DatabaseHandler implements IDatabaseHandler
 		$reply = $this -> execQuery($query);
 		$resultRow = $this -> getResultingRowsOnArrayOfArrays($reply);
 		$this -> closeDatabase($databaseLink, $reply);
-	  	return $resultRow;
+		return $resultRow;
 	}
 
 	/*FOR TRANSACTIONS*/
@@ -273,7 +273,7 @@ class DatabaseHandler implements IDatabaseHandler
 		$reply = $this -> execQueriesInTransaction($queriesOnArray);
 		$resultRow = $this -> getResultingRowOnArray($reply);
 		$this -> closeDatabase($databaseLink, $reply);
-	  	return $resultRow;
+		return $resultRow;
 	}
 
 	public function SQLTransactionAllArray($queriesOnArray)
@@ -282,7 +282,7 @@ class DatabaseHandler implements IDatabaseHandler
 		$reply = $this -> execQueriesInTransaction($queriesOnArray);
 		$resultRow = $this -> getResultingRowsOnArrayOfArrays($reply);
 		$this -> closeDatabase($databaseLink, $reply);
-	  	return $resultRow;    
+		return $resultRow;    
 	}
 	/*FOR TRANSACTIONS*/
 //INTERFACE PUBLIC METHODS
